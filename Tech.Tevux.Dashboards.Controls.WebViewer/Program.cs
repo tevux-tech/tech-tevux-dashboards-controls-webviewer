130d25adbb40901fe049280a92644daf373db8c7
devbotas <simon@devbot.lt>
2023-03-03 11:43:55 +0200

    Updating stuff.

--- Tech.Tevux.Dashboards.Controls.WebViewer/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tech.Tevux.Dashboards.Controls;
-
-class Program {
-    static void Main() {
-        Console.WriteLine("If you can see this, library works.");
-        Console.ReadLine();
-    }
-}
